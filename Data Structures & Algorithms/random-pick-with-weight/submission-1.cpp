15e6b219e534109f5734713d51c49d42c19607c9
SIDDHARTH <siddharth19111@iiitd.ac.in>
2026-08-31 14:46:53 -0400

    Add: random-pick-with-weight - submission-1

---
+++ Data Structures & Algorithms/random-pick-with-weight/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> prefix;
+    Solution(vector<int>& w) {
+        prefix = {0};
+        for(int wt: w)
+            prefix.push_back(prefix.back() + wt);
+    }
+    
+    int pickIndex() {
+        double target = prefix.back() * ((double) rand() / INT_MAX);
+        auto it = upper_bound(prefix.begin(), prefix.end(), target);
+        return it - prefix.begin()-1;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
